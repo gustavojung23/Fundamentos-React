deddec052069756638acb224bf576e423db0f3f3
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2023-12-21 15:12:41 -0300

    create project secret words

---
+++ project-secret-word/src/data/Words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    Carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    Fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    Corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    Computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Mousepad"],
+    Programação: ["jQuery", "Algoritmo", "Framework", "JavaScript", "React"],
+    Alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"]
+};
