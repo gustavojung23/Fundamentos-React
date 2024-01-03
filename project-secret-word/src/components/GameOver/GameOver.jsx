017526b4bf42f55e96d66b218bc63dccc82f22df
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-01-03 18:03:46 -0300

    Update GameOver.jsx

--- project-secret-word/src/components/GameOver/GameOver.jsx
+++ project-secret-word/src/components/GameOver/GameOver.jsx
@@ -1,9 +1,10 @@
 import "./GameOver.css";
 
-const GameOver = ({ retry }) => {
+const GameOver = ({ retry, score }) => {
   return (
     <div className="game_over">
         <h1>Game Over</h1>
+        <h3>A sua pontuação foi: {score} </h3>
         <button onClick={retry}>Resetar Jogo</button>
     </div>
   )
